4ded8dcea3c159fb943c9978b134ea43a1efe4a8
Oleks.Tverdokhlieb <tverdohleb.alex@gmail.com>
2022-06-08 09:45:58 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+WB_API1 = '	1. MjdiN2Q5YTEtYjk1Yy00YzZlLThmZWItNGQyNGE4Y2UxOTI2'
+WB_API2 = '27b7d9a1-b95c-4c6e-8feb-4d24a8ce1926'
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
